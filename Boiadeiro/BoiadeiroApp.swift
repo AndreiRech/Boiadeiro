f9d8e0849110b328123fb0ded045cb93a844318a
AndreiRech <andreirech22@outlook.com>
2025-08-06 16:31:36 -0300

    Initial Commit

---
+++ Boiadeiro/BoiadeiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoiadeiroApp.swift
+//  Boiadeiro
+//
+//  Created by Andrei Rech on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BoiadeiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
